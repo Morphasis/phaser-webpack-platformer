fd855eba7dbeb4c14c79f8e3599cbe64f93cde44
Morphasis <jacksharville@gmail.com>
2017-07-12 00:09:41 +0100

    Updated coin graphic thanks to darklink

--- src/states/Game.js
+++ src/states/Game.js
@@ -20,9 +20,7 @@ export default class extends Phaser.State {
 
     // Platformer images
     game.load.image('player', 'assets/player.png');
-    game.load.image('wall', 'assets/wall.png');
     game.load.image('coin', 'assets/coin.png');
-    game.load.image('enemy', 'assets/enemy.png');
 
     game.load.tilemap('level', 'assets/level.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/Set_01.png');
